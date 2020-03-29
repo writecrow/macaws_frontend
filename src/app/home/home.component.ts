840a5ad424d00cad3e3864676c4c2502169ec95f
markfullmer <mfullmer@gmail.com>
2020-03-29 18:13:26 -0500

    Add dynamic content to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,12 @@ import { AuthorizeComponent } from '../authorize/authorize.component';
 })
 
 export class HomeComponent implements OnInit {
-  total_words : number;
-  total_texts : number;
+  total_words: number;
+  total_texts: number;
+  resourcesTitle: '';
+  resourcesBody: '';
+  whatsNewTitle: '';
+  whatsNewBody: '';
 
   constructor(
     private API: APIService,
@@ -24,11 +28,27 @@ export class HomeComponent implements OnInit {
     this.total_words = 7283469;
     this.total_texts = 8254;
     if (typeof this.route.snapshot.queryParams.auth !== 'undefined') {
-      var auth = this.route.snapshot.queryParams.auth.split(":");
+      var auth = this.route.snapshot.queryParams.auth.split(':');
       if (auth[1]) {
         this.authComponent.authorize(auth[0].toLowerCase(), auth[1], '');
       }
     }
+    this.route.params.subscribe(() => {
+      this.API.getPage('resources-and-tutorials').subscribe(response => {
+        if (response && response[0]) {
+          this.resourcesTitle = response[0].title;
+          this.resourcesBody = response[0].body;
+        }
+      });
+    });
+    this.route.params.subscribe(() => {
+      this.API.getPage('whats-new').subscribe(response => {
+        if (response && response[0]) {
+          this.whatsNewTitle = response[0].title;
+          this.whatsNewBody = response[0].body;
+        }
+      });
+    });
   }
 }
 
