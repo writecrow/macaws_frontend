2bdedfba6ee31c3413708d6b46981fd6568b6c8b
markfullmer <mfullmer@gmail.com>
2023-08-05 15:34:00 -0700

    Add test environment

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://macaws.corporaproject.org',
-  backend: 'https://macaws-api.corporaproject.org/',
+  baseUrl: 'http://test.macaws.corporaproject.org',
+  backend: 'https://test.macaws-api.corporaproject.org/',
   alt_text: '2427f8ce-f19a-4d66-b199-50eb1471928c',
   alt_uuid: 'D1C21A255DCC0DD678DEC8AD54918340',
 };
