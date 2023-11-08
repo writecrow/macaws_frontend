90946cef07e53bef5197fe1813225a17c13bbe46
markfullmer <mfullmer@gmail.com>
2023-11-08 13:54:19 -0700

    Add course descriptions to display

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://test.macaws.corporaproject.org',
+  baseUrl: 'https://test.macaws.corporaproject.org',
   backend: 'https://test.macaws-api.corporaproject.org/',
-  alt_text: '2427f8ce-f19a-4d66-b199-50eb1471928c',
+  alt_text: 'kV8n69Oj2FHBqEkjVBxmlo5s6yvy-ACjX5cEqVkuYfQ',
   alt_uuid: 'D1C21A255DCC0DD678DEC8AD54918340',
 };
