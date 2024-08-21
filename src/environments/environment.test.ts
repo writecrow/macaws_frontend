834f011f9d39f33abab49a3a5dbdf326497faa5e
markfullmer <mfullmer@gmail.com>
2024-08-21 11:50:51 -0700

    Update numbers

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://test.macaws.corporaproject.org',
+  baseUrl: 'https://test.macaws.corporaproject.org/',
   backend: 'https://test.macaws-api.corporaproject.org/',
-  alt_text: 'kV8n69Oj2FHBqEkjVBxmlo5s6yvy-ACjX5cEqVkuYfQ',
+  alt_text: '8695738c-af3c-4ba7-a2f3-90e5db88636b',
   alt_uuid: 'D1C21A255DCC0DD678DEC8AD54918340',
 };
